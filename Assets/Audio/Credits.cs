042fd4b7e12cd6037099d407d962cec91f6a9a76
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-11-26 15:54:56 -0400

    Theme music added for main menu, credits, leaderbs, and level

---
+++ Assets/Audio/Credits.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    /*
+    Main Theme: Catch It by Coma-Media : Music by <a href="https://pixabay.com/users/coma-media-24399569/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=117676">Coma-Media</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=117676">Pixabay</a>
+    Level Theme: The Fall of Man by GabrielDouglas : Music by <a href="https://pixabay.com/users/gabrieldouglas-8149181/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=126118">GabrielDouglas</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=126118">Pixabay</a>
+    Credits Theme: Hybrid Trailer Epic by Alex MakeMusic : Music by <a href="https://pixabay.com/users/alex_makemusic-24186663/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=12954">Alex_MakeMusic</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=12954">Pixabay</a>
+    Leaderboard Theme: Beyond The Time(Main) by Daddy s Music : Music by <a href="https://pixabay.com/users/daddy_s_music-22836301/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=7425">Daddy_s_Music</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=7425">Pixabay</a>
+    
+    */
+}
